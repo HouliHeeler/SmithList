4a1da0d2f0661b878d7fdd4ba41dfe0a2060b46f
HouliHeeler <luke_houlihan@hotmail.com>
2022-11-30 21:37:33 -0800

    build userModel and start userControllers and userRoutes

---
+++ backend/models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please Add a Name'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please Add an email'],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Please Add a Password']
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('User', userSchema)
