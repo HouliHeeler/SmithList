bdae31bda0683be89a53ed80b18569f8d1f92c23
HouliHeeler <luke_houlihan@hotmail.com>
2023-02-21 22:30:30 -0800

    add functionality to total grocery list

--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -1,6 +1,34 @@
 import Controls from "../components/Controls"
+import { FaPlus, FaMinus, FaSave, FaTrash } from 'react-icons/fa';
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { getGoals, reset } from '../features/goals/goalSlice'
 
 function List({setChosenRecipe, chosenRecipe}) {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user } = useSelector((state) => state.auth)
+  const { goals, isError, message } = useSelector(
+    (state) => state.goals
+  )
+
+  useEffect(() => {
+    if (isError) {
+      console.log(message)
+    }
+
+    if (!user) {
+      navigate('/login')
+    }
+
+    dispatch(getGoals())
+
+    return () => {
+      dispatch(reset())
+    }
+  }, [user, navigate, isError, message, dispatch])
 
   let ingredients;
   
@@ -10,6 +38,37 @@ function List({setChosenRecipe, chosenRecipe}) {
     ingredients = componentArray.map((item, i) => {
       return <div key={i}>{item}</div>
     })
+  }
+
+  const [addOns, setAddOns] = useState("")
+
+  const onChange = (e) => {
+    setAddOns(e.target.value)
+  }
+
+  const firstDraft = []
+  goals.map(recipe => recipe.text[0].sections.map(step => step.components.map(item => firstDraft.push(item.raw_text))))
+
+  const [finalDraft, setFinalDraft] = useState(firstDraft)
+
+  const shoppingList = finalDraft.map((item, i) => {
+    return (
+      <div className="grocery-item" >
+        <FaMinus 
+          className="delete"
+          onClick={() => removeItem(item)}/>
+        <div key={i}>{item}</div>
+      </div>
+    )
+  })
+
+  function addItem() {
+    setFinalDraft(prevList => ([addOns, ...prevList]))
+    setAddOns('')
+  }
+
+  function removeItem(item) {
+    setFinalDraft(finalDraft.filter(el => el !== item))
   }
 
   return (
@@ -26,7 +85,26 @@ function List({setChosenRecipe, chosenRecipe}) {
         <div className="page-instructions">
           <h3>Total Ingredients</h3>
           <div className="recipe-instructions">
-
+            {shoppingList}
+          </div>
+          <div className="add-groceries">
+            <FaSave />
+            <div>
+              <input
+                type="text"
+                id="addOn"
+                name="addOn"
+                placeholder="Add groceries..." 
+                value={addOns}
+                onChange={onChange} 
+                onKeyPress={event => {
+                  if (event.key === 'Enter') {
+                    addItem()
+                  }
+                }}/>
+              <FaPlus className='add' onClick={addItem}/>
+            </div>
+            <FaTrash />
           </div>
         </div>
       </section>
